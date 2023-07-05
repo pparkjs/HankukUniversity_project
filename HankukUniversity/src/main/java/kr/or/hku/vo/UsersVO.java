85e6a96dfe8ef20dadaa5ef79966b849d7edc22b
jihyun825 <wlgus361@gmail.com>
2023-07-05 17:44:41 +0900

    feat:사용주체 VO작성

---
+++ HankukUniversity/src/main/java/kr/or/hku/vo/UsersVO.java
@@ -0,0 +1,15 @@
+package kr.or.hku.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UsersVO {
+	private String userNo; //사용자식별번호
+	private String userPw; //사용자비밀번호
+	private String userClsCd; //사용자구분코드
+	private int enabled; //스프링시큐리티
+}
