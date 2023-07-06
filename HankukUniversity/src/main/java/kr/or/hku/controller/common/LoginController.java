96736bf1976fa7e592099674bbcdbaebc7a371a8
jihyun825 <wlgus361@gmail.com>
2023-07-06 11:07:18 +0900

    feat:loginController 수정

--- HankukUniversity/src/main/java/kr/or/hku/controller/common/LoginController.java
+++ HankukUniversity/src/main/java/kr/or/hku/controller/common/LoginController.java
@@ -23,35 +23,71 @@ public class LoginController {
 	@Autowired
 	private ILoginService loginService;
 	
+	
+	//로그인페이지 호출 
 	@GetMapping("/login")
 	public String loginPage() {
+		
 		return "login/loginPage";
 	}
 	
+	//로그인 처리 로직	
 	@PostMapping(value="/login")
 	public String loginProcess(Model model,UsersVO usersVo,HttpServletRequest request) {
 		String goPage = "";
 		HttpSession session = request.getSession();
 		UsersVO userVo = loginService.loginUser(usersVo);
+		System.out.println("userVO"+usersVo.getUserClsCd());
 		model.addAttribute("cls", userVo.getUserClsCd());
 		
-
+		//사용자식별코드가 학생일경우
 		if(userVo.getUserClsCd().equals("student")) {
 			StudentVO stdVo = loginService.studentUser(userVo.getUserNo());
 			session.setAttribute("std", stdVo);
 	
-			goPage = "portal/blank";
-		}else if(userVo.getUserClsCd().equals("professor")) {
+			goPage = "redirect:/main/portal";
+		}else if(userVo.getUserClsCd().equals("professor")) {//사용자식별코드가 교수일경우
 			ProfessorVO proVo = loginService.professorUser(userVo.getUserNo());
 			session.setAttribute("pro", proVo);
-			goPage = "admin/blank";
-		}else{
+			goPage = "redirect:/main/pro";
+		}else{//사용자식별코드가 직원일경우
 			EmployeeVO empVo = loginService.employeeUser(usersVo.getUserNo());
-			session.setAttribute("emp", userVo);
-			goPage = "professor/blank";
+			session.setAttribute("emp", empVo);
+			goPage = "redirect:/main/emp";
 		}
 		
 		return goPage;
 	}
+	
+	//로그아웃 처리로직
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		
+		session.invalidate();
+		return "redirect:/main/login";
+	}
+	
+	//포탈페이지 호출 
+	@GetMapping("/portal")
+	public String goPortal() {
+		return "portal/home";
+	}
+	
+	@GetMapping("/emp")
+	public String adminMain() {
+		return "admin/main";
+	}
+	
+	@GetMapping("/pro")
+	public String professorMain() {
+		return "professor/main";
+	}
+	
+	
+	@GetMapping("/student")
+	public String studentMain() {
+		return "student/main";
+	}
+
 
 }
