57a73186ba9b1b66b768b2932f84a245c65a84f6
pparkjs <qkrwjdtn741@naver.com>
2023-07-07 20:41:57 +0900

    feat: 공통코드에 대한 mapper 파일 추가

--- HankukUniversity/src/main/java/kr/or/hku/mapper/student/CourseInfoMapper.java
+++ HankukUniversity/src/main/java/kr/or/hku/mapper/student/CourseInfoMapper.java
@@ -1,12 +1,15 @@
 package kr.or.hku.mapper.student;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kr.or.hku.vo.DepartmentVO;
 import kr.or.hku.vo.SubjectVO;
 
 @Mapper
 public interface CourseInfoMapper {
-	public List<SubjectVO> getList();
+	public List<SubjectVO> getList(Map<String, Object> map);
+	public List<DepartmentVO> getAllDept();
 }
