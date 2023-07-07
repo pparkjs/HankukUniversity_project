782922b937d9ed61aeef98ee941739bbff66b99b
pparkjs <qkrwjdtn741@naver.com>
2023-07-07 20:40:46 +0900

    feat: 공통코드 VO 추가

---
+++ HankukUniversity/src/main/java/kr/or/hku/vo/CommonVO.java
@@ -0,0 +1,16 @@
+package kr.or.hku.vo;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CommonVO {
+	 private String comCd;
+	 private String comCdGrp;
+	 private String comCdNm;
+}
