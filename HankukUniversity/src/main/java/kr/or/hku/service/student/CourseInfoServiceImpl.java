8e93039462e7e1ca56b9e7630f8da01563c612c6
pparkjs <qkrwjdtn741@naver.com>
2023-07-07 20:41:31 +0900

    feat: 공통코드에 대한 service 추가

--- HankukUniversity/src/main/java/kr/or/hku/service/student/CourseInfoServiceImpl.java
+++ HankukUniversity/src/main/java/kr/or/hku/service/student/CourseInfoServiceImpl.java
@@ -1,11 +1,13 @@
 package kr.or.hku.service.student;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.or.hku.mapper.student.CourseInfoMapper;
+import kr.or.hku.vo.DepartmentVO;
 import kr.or.hku.vo.SubjectVO;
 
 @Service
@@ -15,8 +17,13 @@ public class CourseInfoServiceImpl implements ICourseInfoService {
 	private CourseInfoMapper courseMapper;
 	
 	@Override
-	public List<SubjectVO> getList() {
-		return courseMapper.getList();
+	public List<SubjectVO> getList(Map<String, Object> map) {
+		return courseMapper.getList(map);
+	}
+
+	@Override
+	public List<DepartmentVO> getAllDept() {
+		return courseMapper.getAllDept();
 	}
 
 }
