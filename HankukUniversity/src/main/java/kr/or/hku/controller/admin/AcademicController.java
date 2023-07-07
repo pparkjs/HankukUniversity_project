3b4266667462b83b587e7fe3f639454e206b0280
kyw95 <kyw0300@naver.com>
2023-07-07 09:11:42 +0900

    feat:학사관리 컨트롤러 생성

--- HankukUniversity/src/main/java/kr/or/hku/controller/admin/AcademicController.java
+++ HankukUniversity/src/main/java/kr/or/hku/controller/admin/AcademicController.java
@@ -1,12 +1,17 @@
 package kr.or.hku.controller.admin;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.or.hku.service.admin.IAcademicAdm;
 import kr.or.hku.vo.DepartmentVO;
@@ -28,5 +33,18 @@ public class AcademicController {
 		model.addAttribute("deptList",deptList);
 		
 		return "admin/dept-administration";
+	}
+	
+	@DeleteMapping("/dept-administration")
+	@ResponseBody
+	public ResponseEntity<String> deptDelete(
+			@RequestBody HashMap<String, String> data
+			){
+		log.info("deptDelete() 실행...!");
+		for(int i=0; i<data.size(); i++) {
+			
+		}
+		log.info("data : "+data);
+		return null;
 	}
 }
