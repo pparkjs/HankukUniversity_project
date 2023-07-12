2f22e9073f7d10fd6f3c4b929cde3d65cc2e1958
pparkjs <qkrwjdtn741@naver.com>
2023-07-12 17:37:50 +0900

    feat: 공통 서비스추가

---
+++ HankukUniversity/src/main/java/kr/or/hku/common/service/impl/CommonFileServiceImpl.java
@@ -0,0 +1,78 @@
+package kr.or.hku.common.service.impl;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import kr.or.hku.common.mapper.CommonFileMapper;
+import kr.or.hku.common.service.CommonFileService;
+import kr.or.hku.common.vo.AttachFileVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class CommonFileServiceImpl implements CommonFileService {
+
+	@Autowired
+	private CommonFileMapper fileMapper;
+
+	// 업로드 패스
+	@Resource(name = "uploadPath")
+	private String resourcePath;
+
+	@Override
+	public int getAttachFileNo() {
+		return fileMapper.getAttachFileNo();
+	}
+
+	@Override
+	public int insertFile(MultipartFile file, int attachFileNo,int i) {
+		String uploadPath = "c:" + resourcePath;
+		File folder = new File(uploadPath);
+		if(!folder.exists()) {
+			folder.mkdirs();
+		}
+		// 랜덤 uuid 생성
+		String uuid = UUID.randomUUID().toString();
+		// uuid + 오리지날 네임 결합
+		String saveFileName = "/" + uuid + "_" + file.getOriginalFilename();
+		// 업로드 패스 저장
+		uploadPath = "c:" + resourcePath + saveFileName;
+		
+		try {
+			file.transferTo(new File(uploadPath));
+		} catch (IllegalStateException | IOException e) {
+			log.error(e.getMessage());
+		}
+		AttachFileVO fileVO = new AttachFileVO();
+		fileVO.setAtchFileNo(attachFileNo);
+		fileVO.setAtchFileSeq(i);
+		fileVO.setFileContType(file.getContentType());
+		fileVO.setFileOrgnlFileNm(file.getOriginalFilename());
+		fileVO.setFilePath(saveFileName);
+		fileVO.setFileSize(FileUtils.byteCountToDisplaySize(file.getSize()));
+		
+		int cnt = fileMapper.insertFile(fileVO);
+		return cnt;
+	}
+	
+	// 공지사항 파일 가져오기
+	@Override
+	public List<AttachFileVO> getNoticeFile(int atchFileNo) {
+		return fileMapper.getNoticeFile(atchFileNo);
+	}
+	
+	@Override
+	public int deleteFile(int fileNo) {
+		return fileMapper.deleteFile(fileNo);
+	}
+
+}
