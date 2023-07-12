fb8411fb8dd2bd523ed35ba263dd91a6d0d91f37
pparkjs <qkrwjdtn741@naver.com>
2023-07-12 17:37:25 +0900

    feat: 공통 mapper 추가

--- HankukUniversity/src/main/java/kr/or/hku/common/mapper/CommonMapper.java
+++ HankukUniversity/src/main/java/kr/or/hku/common/mapper/CommonMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kr.or.hku.admin.vo.FacilityVO;
 import kr.or.hku.common.vo.CommonVO;
 import kr.or.hku.student.vo.StudentVO;
 
@@ -11,4 +12,5 @@ import kr.or.hku.student.vo.StudentVO;
 public interface CommonMapper {
 	public List<CommonVO> getAllCommonData();
 	public StudentVO myAllInfo(String stdNo);
+	public List<FacilityVO> floorListFlct(String flctNo);
 }
