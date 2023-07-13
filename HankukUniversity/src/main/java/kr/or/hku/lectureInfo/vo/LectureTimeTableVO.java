c86364979594d441c7582e87ac449fec1ffebe42
jihyun825 <wlgus361@gmail.com>
2023-07-13 09:07:00 +0900

    feat:강의 시간표 불러오는 vo작성

---
+++ HankukUniversity/src/main/java/kr/or/hku/lectureInfo/vo/LectureTimeTableVO.java
@@ -0,0 +1,24 @@
+package kr.or.hku.lectureInfo.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class LectureTimeTableVO {
+	
+	
+    private String lecapNo    ;
+    private String lecscDay   ;
+    private String periodCd   ;
+    private String lecscHour  ;
+    private String subNm      ;
+    private String proNm      ;
+    
+    
+    
+     
+
+}
