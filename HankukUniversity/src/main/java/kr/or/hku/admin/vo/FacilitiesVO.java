afa9a3b3edce2a6fc5924f8f0f96447c3c1dfeb4
pparkjs <qkrwjdtn741@naver.com>
2023-07-14 09:12:02 +0900

    feat: 시설예약을 위해 조인 필드 추가

--- HankukUniversity/src/main/java/kr/or/hku/admin/vo/FacilitiesVO.java
+++ HankukUniversity/src/main/java/kr/or/hku/admin/vo/FacilitiesVO.java
@@ -8,9 +8,19 @@ import lombok.ToString;
 @Setter
 @ToString
 public class FacilitiesVO {
-
 	private String flctsNo;
 	private String flctNo;
 	private String flctsNm;
-		
+	
+	// 조인해서 가져올 필드
+	private String flctNm; //시설이름
+	private String flctsRsvtNo; // 시설물예약번호
+	private String stdNo;		//예약자 학번
+	private String rsvtDate;	//예약날짜
+	private String startHours;	//시작날짜
+	private String endHours;	//종료날짜
+	private int useUsers;	//사용인원
+	private String usePrps;	//사용목적
+	
+	private String stdNm;		//예약자 학번
 }
