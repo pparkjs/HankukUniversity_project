a69a33664b10b77584c9d70da31aeca2bd8a889a
jihyun825 <wlgus361@gmail.com>
2023-07-17 09:01:26 +0900

    feat_휴강신청 서비스(인터페이스), 서비스 구현체 작성

---
+++ HankukUniversity/src/main/java/kr/or/hku/professor/serviceImpl/RestClassServiceImpl.java
@@ -0,0 +1,42 @@
+package kr.or.hku.professor.serviceImpl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.or.hku.ServiceResult;
+import kr.or.hku.lectureInfo.vo.LectureTimeTableVO;
+import kr.or.hku.professor.mapper.RestClassMapper;
+import kr.or.hku.professor.service.RestClassService;
+@Service
+public class RestClassServiceImpl implements RestClassService {
+	
+	@Autowired
+	RestClassMapper restClassMapper;
+
+	@Override
+	public List<LectureTimeTableVO> getMyTimetable(String proNo) {
+		// TODO Auto-generated method stub
+		return restClassMapper.getMyTimetable(proNo);
+	}
+	
+	@Override
+	public LectureTimeTableVO setRestTable(Map<String,String> myMap) {
+		LectureTimeTableVO myTable = restClassMapper.setRestTable(myMap);
+		return myTable;
+	}
+	
+	@Override
+	public ServiceResult insertRestTable(Map<String, String> myMap) {
+		int res = restClassMapper.insertRestTable(myMap);
+		ServiceResult result;
+		if(res > 0) {
+			result = ServiceResult.OK;
+		}else {
+			result = ServiceResult.FAILED;
+		}
+		return result;
+	}
+}
