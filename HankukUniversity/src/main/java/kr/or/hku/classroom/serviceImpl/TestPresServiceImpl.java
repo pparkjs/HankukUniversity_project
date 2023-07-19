6b7e81c87b8f93e1d951f67750eb11dc6f7cbc08
pparkjs <qkrwjdtn741@naver.com>
2023-07-19 14:53:28 +0900

    feat: 시험지 출제 및 수정 service 추가

---
+++ HankukUniversity/src/main/java/kr/or/hku/classroom/serviceImpl/TestPresServiceImpl.java
@@ -0,0 +1,121 @@
+package kr.or.hku.classroom.serviceImpl;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.or.hku.ServiceResult;
+import kr.or.hku.classroom.mapper.TestPresMapper;
+import kr.or.hku.classroom.service.TestPresService;
+import kr.or.hku.classroom.vo.TestAnswerVO;
+import kr.or.hku.classroom.vo.TestVO;
+
+@Service
+public class TestPresServiceImpl implements TestPresService {
+	
+	@Autowired
+	private TestPresMapper testPresMapper;
+	
+	@Transactional(rollbackFor = SQLException.class)
+	@Override
+	public ServiceResult testInsert(TestVO test) {
+		ServiceResult result = null;
+		
+		// 이미 출제한 시험인지 체크
+		int check = testPresMapper.testCheck(test);
+		
+		if(check > 0) {
+			result = ServiceResult.EXIST;
+		}else {
+			
+			// 시험 테이블에 등록
+			int cnt = testPresMapper.testInsert(test);
+			
+			for(TestAnswerVO answer : test.getAnswerList()) {
+				answer.setLecapNo(test.getLecapNo());
+				answer.setTestNo(test.getTestNo());
+				testPresMapper.testAnswerInsert(answer);
+			}
+			
+			if(cnt > 0) {
+				result = ServiceResult.OK;
+			}else {
+				result = ServiceResult.FAILED;
+			}
+			
+		}
+		
+		return result;
+	}
+	
+	@Override
+	public int testCheck(TestVO test) {
+		return testPresMapper.testCheck(test);
+	}
+
+	@Override
+	public TestVO testDetail(TestVO test) {
+		TestVO vo = testPresMapper.testDetail(test);
+		if(vo.getTestSe().equals("middle")) {
+			vo.setTestSe("중간고사");
+		}else {
+			vo.setTestSe("기말고사");
+		}
+		
+		TestVO testVO = timeChange(vo);
+		
+		return testVO;
+	}
+	
+
+	@Transactional(rollbackFor = SQLException.class)
+	@Override
+	public ServiceResult testUpdate(TestVO test) {
+		ServiceResult result = null;
+		// 시험 테이블 수정
+		int cnt = testPresMapper.testUpdate(test);
+		
+		
+		// 먼저 정답테이블에 있는거 삭제후 수정 이루어짐
+		testPresMapper.testAnswerDelete(test.getTestNo());
+		
+		for(TestAnswerVO answer : test.getAnswerList()) {
+			answer.setLecapNo(test.getLecapNo());
+			answer.setTestNo(test.getTestNo());
+			testPresMapper.testAnswerInsert(answer);
+		}
+		
+		if(cnt > 0) {
+			result = ServiceResult.OK;
+		}else {
+			result = ServiceResult.FAILED;
+		}
+			
+		
+		return result;
+	}
+	
+	// 초합을 시 분 초 로 나누기
+	public TestVO timeChange(TestVO vo) {
+		int totalSeconds = vo.getTestTimeLimit();
+
+		int hours = totalSeconds / 3600;
+		int minutes = (totalSeconds % 3600) / 60;
+		int seconds = totalSeconds % 60;
+
+		System.out.println("시: " + hours);
+		System.out.println("분: " + minutes);
+		System.out.println("초: " + seconds);
+		
+		vo.setHours(hours);
+		vo.setMinutes(minutes);
+		vo.setSeconds(seconds);
+		
+		return vo;
+	}
+
+
+}
