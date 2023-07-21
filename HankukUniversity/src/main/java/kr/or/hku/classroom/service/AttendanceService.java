d3b97bfacf0e01200cdf17a7a2a74cf906f4510a
sunghee95 <sunghee9647@gmail.com>
2023-07-21 15:24:34 +0900

    feat: 교수 클래스룸 출결 관리 기능 구현

--- HankukUniversity/src/main/java/kr/or/hku/classroom/service/AttendanceService.java
+++ HankukUniversity/src/main/java/kr/or/hku/classroom/service/AttendanceService.java
@@ -6,17 +6,24 @@ import kr.or.hku.classroom.vo.AttendanceVO;
 
 public interface AttendanceService {
 	
+	// ------------------------ 교수 ----------------------------//  
 	// 출석 관리
 	public AttendanceVO manageAttendance(AttendanceVO vo);
 	
 	// 해당 과목 수강하는 학생 리스트 
 	public List<AttendanceVO> getStdList(String lecapNo);
+
+	public List<AttendanceVO> getStdAttendanceList(AttendanceVO vo);
+	
+	// 출석 변경 
+	public int updateAttendance(AttendanceVO vo);
+
 	
 	
 	
+// ------------------------ 학생 ----------------------------//  
 	
-	
-// ------------------------ 학생 출석 이의신청 ----------------------------//  
+
 //	public List<AttendanceVO> dmrList();
 //	
 //	public AttendanceVO dmrDetail(String stdNo);
