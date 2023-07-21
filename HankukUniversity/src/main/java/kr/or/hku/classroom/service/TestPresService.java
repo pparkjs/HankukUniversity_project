72008e3b8b63452a834f2767f007da0b903b7625
pparkjs <qkrwjdtn741@naver.com>
2023-07-21 15:48:11 +0900

    feat: 시험 응시하기 service 추가

--- HankukUniversity/src/main/java/kr/or/hku/classroom/service/TestPresService.java
+++ HankukUniversity/src/main/java/kr/or/hku/classroom/service/TestPresService.java
@@ -1,7 +1,12 @@
 package kr.or.hku.classroom.service;
 
 
+import java.util.List;
+import java.util.Map;
+
 import kr.or.hku.ServiceResult;
+import kr.or.hku.classroom.vo.StudentAnswerVO;
+import kr.or.hku.classroom.vo.TestAnswerVO;
 import kr.or.hku.classroom.vo.TestVO;
 
 public interface TestPresService {
@@ -10,4 +15,13 @@ public interface TestPresService {
 	public TestVO testDetail(TestVO test);
 	public TestVO timeChange(TestVO test);
 	public ServiceResult testUpdate(TestVO testVO);
+	public List<TestVO> getTestList(String stdNo);
+	public List<TestAnswerVO> getAnswerList(TestVO test);
+	public int getMaxCh(TestVO test);
+	public int testTakeInsert(TestVO test);
+	public void studentTestAnswerInsert(StudentAnswerVO sAnsVO);
+	public int scoreUpdate(Map<String, Object> map);
+	public int preTestCheck(Map<String, Object> map);
+	public TestVO getTtNo(TestVO test);
+	public List<StudentAnswerVO> getStuAnsList(int ttNo);
 }
