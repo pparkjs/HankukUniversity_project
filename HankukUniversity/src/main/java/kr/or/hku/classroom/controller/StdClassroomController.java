c3463e57a42aca8402ac45df5c9300d6f4f1bb57
jihyun825 <wlgus361@gmail.com>
2023-07-25 15:53:06 +0900

    feat:학생클래스룸 공지사항 기능구현완료

--- HankukUniversity/src/main/java/kr/or/hku/classroom/controller/StdClassroomController.java
+++ HankukUniversity/src/main/java/kr/or/hku/classroom/controller/StdClassroomController.java
@@ -17,9 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import kr.or.hku.classroom.service.AssignmentService;
 import kr.or.hku.classroom.service.AttendanceService;
 import kr.or.hku.classroom.service.ClassroomService;
+import kr.or.hku.classroom.service.LectureNoticeService;
 import kr.or.hku.classroom.vo.AssignmentVO;
 import kr.or.hku.classroom.vo.AttendanceVO;
 import kr.or.hku.classroom.vo.ClassroomVO;
+import kr.or.hku.classroom.vo.LectureNoticeVO;
+import kr.or.hku.common.service.CommonFileService;
+import kr.or.hku.common.vo.AttachFileVO;
 import kr.or.hku.student.vo.StudentVO;
 import lombok.extern.slf4j.Slf4j;
 
@@ -36,13 +40,14 @@ public class StdClassroomController {
 	
 	@Autowired
 	private AttendanceService attendanceService;
-	
-	
+	@Autowired
+	private LectureNoticeService noticeService;
+	@Autowired
+	private CommonFileService fileService;
 	// 클래스룸 리스트 
 	@GetMapping("/stdClassroomList")
 	public String classroomList(Model model, HttpServletRequest request) {
 		log.info("classroomList 실행 !");
-		
 		HttpSession session = request.getSession();
 		StudentVO stdVo = (StudentVO) session.getAttribute("std");
 		List<ClassroomVO> list = classService.stdSelectList(stdVo.getStdNo());
@@ -70,9 +75,11 @@ public class StdClassroomController {
 		// 공지사항, 과제, 출석 정보를 맵에 담은 변수를 통해 가져옴 
 		List<AssignmentVO> list = classService.stdMainAssignList(map);
 		List<AttendanceVO> attendList = classService.getMyAttendance(map);
+		List<LectureNoticeVO> noticeList = noticeService.getNoticeList(lecapNo);
 		log.info("list!!!!!!!=>" + list.toString());
 		model.addAttribute("list", list);
 		model.addAttribute("attendList", attendList);
+		model.addAttribute("noticeList", noticeList);
 		return "student/classroomMain";
 	}
 	
@@ -92,5 +99,24 @@ public class StdClassroomController {
 								, Model model) {
 		return "student/attendanceDmr";
 	}
+	
+	@GetMapping("/noticeList")
+	public String noticeList(HttpSession session, Model model) {
+		String lecapNo = session.getAttribute("lecapNo").toString();
+		List<LectureNoticeVO> noticeList = noticeService.getNoticeList(lecapNo);
+		model.addAttribute("noticeList", noticeList);
+		return "student/lectureNoticeList";
+	}
+	
+	//학생클래스룸 공지상세 페이지
+		@GetMapping("/detailNotice/{lecntNo}")
+		public String detailNotice(@PathVariable int lecntNo,Model model) {
+			LectureNoticeVO noticeVO = noticeService.getNotcieDetail(lecntNo);
+			List<AttachFileVO> fileList = fileService.getFileList(noticeVO.getAtchFileNo());
+			noticeVO.setFileList(fileList);
+			log.info("디테일가져왔냐!!!!!"+noticeVO.toString());
+			model.addAttribute("noticeVO",noticeVO);
+			return "student/lectureNoticeDetail";
+		}
 	
 }
--- HankukUniversity/src/main/webapp/WEB-INF/views/professor/lectureNoticeForm.jsp
+++ HankukUniversity/src/main/webapp/WEB-INF/views/professor/lectureNoticeForm.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/security/tags" prefix="sec" %>
 <div class="content-body">
 	<div class="container-fluid">
 		<!-- row -->
@@ -75,6 +76,7 @@
 				</div>
 				<input type="hidden" name="lecntWriter" value="${pro.proNm}">
 				<input type="hidden" name="lecapNo" value="${lecapNo}">
+				<sec:csrfInput/>
 			</form>
 		</div>
 	</div>
