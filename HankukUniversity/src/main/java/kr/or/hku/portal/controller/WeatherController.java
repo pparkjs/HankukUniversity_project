7e272a7798135de8dabe1bdf9f9f765400ad7080
jihyun825 <wlgus361@gmail.com>
2023-07-26 10:27:37 +0900

    feat : 날씨 api키 데이터베이스 연동 완료

--- HankukUniversity/src/main/java/kr/or/hku/portal/controller/WeatherController.java
+++ HankukUniversity/src/main/java/kr/or/hku/portal/controller/WeatherController.java
@@ -1,15 +1,24 @@
 package kr.or.hku.portal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.or.hku.portal.service.WeatherService;
 
 @Controller
 @RequestMapping("/weather")
 public class WeatherController {
 	
+	@Autowired
+	WeatherService weatherService;
+	
 	@GetMapping("/test")
-	public String goWeather() {
+	public String goWeather(Model model) {
+		String key = weatherService.getApiKey();
+		model.addAttribute("key", key);
 		return "weather";
 	}
 
