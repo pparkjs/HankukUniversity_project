d8609c962fc90cbb7c232a99d61e86d181be8192
sunghee95 <sunghee9647@gmail.com>
2023-08-03 20:16:53 +0900

    feat: 학생 마이페이지 기능 구현

--- HankukUniversity/src/main/java/kr/or/hku/mypage/service/IMypageService.java
+++ HankukUniversity/src/main/java/kr/or/hku/mypage/service/IMypageService.java
@@ -1,9 +1,18 @@
 package kr.or.hku.mypage.service;
 
+import java.util.Map;
+
+import kr.or.hku.ServiceResult;
+import kr.or.hku.login.vo.UsersVO;
 import kr.or.hku.student.vo.StudentVO;
+
 
 public interface IMypageService {
 
 	public StudentVO detailInfo(String stdNo);
-	public int updateInfo(StudentVO studentVO);
+	public int myInfo(StudentVO studentVO);
+	public boolean checkPassword(UsersVO usersVO);
+	public int modifyInfo(StudentVO vo);
+	public int changePassword(Map<String, String> map);
+	public ServiceResult updateProfile(StudentVO vo);
 }
