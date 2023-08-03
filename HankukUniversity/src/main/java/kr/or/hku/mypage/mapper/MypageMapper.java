afb55b108c95d528881f4d0b98f38393ea6c6771
sunghee95 <sunghee9647@gmail.com>
2023-08-03 17:50:18 +0900

    feat: 학생 마이페이지 기능 구현

--- HankukUniversity/src/main/java/kr/or/hku/mypage/mapper/MypageMapper.java
+++ HankukUniversity/src/main/java/kr/or/hku/mypage/mapper/MypageMapper.java
@@ -1,14 +1,22 @@
 package kr.or.hku.mypage.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import kr.or.hku.ServiceResult;
+import kr.or.hku.login.vo.UsersVO;
 import kr.or.hku.student.vo.StudentVO;
 
 
 @Mapper
-public interface StdMypageMapper {
+public interface MypageMapper {
 	
 	public StudentVO detailInfo(String stdNo);
-	public int updateInfo(StudentVO studentVO);
+	public int myInfo(StudentVO studentVO);
+	public UsersVO loginUser(String userNo);
+	public int modifyInfo(StudentVO vo);
+	public int changePassword(Map<String, String> map);
+	public ServiceResult updateProfile(StudentVO vo);
 	
 }
